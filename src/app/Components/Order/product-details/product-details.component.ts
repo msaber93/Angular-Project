c5de0ee3ed6d72efdf9275549637355ef421a1d4
Mahmoud Saber <engabosaber2014@gmail.com>
2023-05-01 02:47:55 +0200

    Routing - ActivateRoute service

--- src/app/Components/Order/product-details/product-details.component.ts
+++ src/app/Components/Order/product-details/product-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/Models/iproduct';
+import { StaticProductsService } from 'src/app/Services/static-products.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  reciviedPrdID: number = 0;
+  prd : IProduct | null = null;
+  constructor(private activtedRoute: ActivatedRoute
+      , private prdService: StaticProductsService, private location: Location) {
 
+  }
+
+
+  ngOnInit(): void {
+    this.reciviedPrdID = Number(this.activtedRoute.snapshot.paramMap.get('pid'));
+    this.prd = this.prdService.getProductByID(this.reciviedPrdID);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
